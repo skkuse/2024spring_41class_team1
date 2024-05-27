b2085825176930205c12107a574f776a1edc0c90
codream <codream00@gmail.com>
2024-05-27 10:12:48 +0900

    refactor: apply @Setter, @Getter
    - Streamlined with Lombok's @Setter and @Getter annotations

--- backend/src/main/java/com/skku/BitCO2e/DTO/UserDTO.java
+++ backend/src/main/java/com/skku/BitCO2e/DTO/UserDTO.java
@@ -1,27 +1,16 @@
 package com.skku.BitCO2e.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDTO {
     private String username;
     private String email;
 
     public UserDTO(String username, String email) {
         this.username = username;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
