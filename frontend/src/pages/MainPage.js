40e4172f4418b9d8b85d1fe7cbe0ba7cb64de8e5
Chan Lee <chan1122@g.skku.edu>
2024-05-31 00:20:54 +0900

    main page function: CopyToClipboard has implemented

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Header from '../components/Header';
 import CodeField from '../components/CodeField';
 import { Button, styled, Box } from '@mui/material';
+import { FileUpload } from '@mui/icons-material';
 
 const Section = styled(Box)({
   minHeight: 30,
   padding: 20,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const Bulletin = styled(Box)({
+  margin: 10,
+  width: 1000,
+  height: 250,
+  background: 'lightgray',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
@@ -22,15 +33,26 @@ const RefactoringArea = styled(Box)({
   flexDirection: 'column',
 });
 
+const AdBanner = styled(Box)({
+  margin: 50,
+  width: 150,
+  height: 600,
+  background: 'lightgray',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
 const DropBox = styled(Box)({
   height: 240,
   border: '2px dashed #dee2e6',
   borderRadius: 5,
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  userSelect: 'none',
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  
 });
 
 const UploadButton = styled(Button)({
@@ -40,6 +62,7 @@ const UploadButton = styled(Button)({
   border: '2px solid #dee2e6',
   borderRadius: 5,
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  fullWidth: true,
 });
 
 const ConvertButton = styled(Button)({
@@ -47,6 +70,7 @@ const ConvertButton = styled(Button)({
   border: '1px solid #dee2e6',
   borderRadius: 5,
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  fullWidth: true,
 });
 
 const CopyButton = styled(Button)({
@@ -54,33 +78,81 @@ const CopyButton = styled(Button)({
   border: '1px solid #dee2e6',
   borderRadius: 5,
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  fullWidth: true,
 });
 
+
+
 const MainPage = () => {
+
+  const text = useState('');
+  const editorRef = useRef(null);
+
+  const OnPageLoad = () => {
+
+  };
+  
+  const BrowseFile = () => {
+    
+  };
+
+  const Convert = async () => {
+    try { /*still developing...*/
+      const response = await fetch('/refactoring', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+        body: text,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.text();
+    } catch (error) {
+      alert("Refactoring에 실패하였습니다.\n잠시 후 다시 시도해주세요.");
+    }
+  };
+
+  const CopyToClipboard = (text) => {
+    console.log(editorRef);
+    navigator.clipboard.writeText(editorRef.current.editor.getValue())
+      .then(() => {
+        alert("클립보드에 성공적으로 복사하였습니다.");
+      })
+      .catch((error) => {
+        alert("클립보드 복사에 실패하였습니다. 잠시 후 다시 시도해주세요.");
+      })
+  };
+
   return (
     <div>
       <Header />
         <Section>
-          {/*Still developing...*/}
+          <Bulletin>Bulletin</Bulletin>
         </Section>
 
       <Section>
+        <AdBanner>Ad_1</AdBanner>
         <Container>
           <RefactoringArea>
-            <DropBox>Drag and drop your source code!</DropBox>
-            <UploadButton>...or browse your file</UploadButton>
+            <DropBox><FileUpload fontSize='large'></FileUpload><p>Drag and drop your source code!</p></DropBox>
+            <UploadButton onClick={BrowseFile}>...or browse your file</UploadButton>
             <CodeField readOnly={false} theme='terminal' placeholder='Input your source code here.'></CodeField>
-            <ConvertButton fullWidth variant="contained" color="primary">Convert!</ConvertButton>
-            <CodeField readOnly={true} theme='mono_industrial' placeholder='Refactored code will be here.'></CodeField>
-            <CopyButton fullWidth variant="contained" color="primary">Copy to your clipboard</CopyButton>
+            <ConvertButton variant="contained" color="primary" onClick={Convert}>Convert!</ConvertButton>
+            <CodeField readOnly={true} theme='mono_industrial' placeholder='Refactored code will be here.' ref={editorRef}></CodeField>
+            <CopyButton variant="contained" color="primary" onClick={CopyToClipboard}>Copy to your clipboard</CopyButton>
           </RefactoringArea>
-          {/*<AdBanner></AdBanner>*/}
         </Container>
+        <AdBanner>Ad_2</AdBanner>
       </Section>
       
-      <div className='section'>
-        {/*<InformationArea></InformationArea>*/}
-      </div>
+      <Section>
+        <div style={{width:1000, height:600, background:"lightgray"}}></div> {/*임시 자리 표시*/}
+        {/*<ServerInfo></ServerInfo><Analysis></Analysis>*/}
+      </Section>
       
       {/*<Footer>*Copyright, email, info, etc. comes here.*"</Footer>*/}
     </div>
